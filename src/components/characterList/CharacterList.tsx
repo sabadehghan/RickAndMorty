50a7bc897da73d0c63b7485df836e281251703c5
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-05 20:00:32 +0430

    implement getData and display data in page

--- src/components/characterList/CharacterList.tsx
+++ src/components/characterList/CharacterList.tsx
@@ -5,18 +5,45 @@ import {
   OutlinedInput,
   InputAdornment,
   IconButton,
+  Grid,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import { useTheme } from "@emotion/react";
 import FilterDialog from "../filterDialog/FilterDialog";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/client";
+import { Character } from "../../constants/GlobalTypes";
+import background from "../../assets/images/background.png";
+const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      results {
+        id
+        name
+        image
+        species
+        gender
+        status
+      }
+    }
+  }
+`;
 
 export default function CharacterList() {
   const theme: any = useTheme();
 
-  
   /////State//////
   const [openFilterDialog, setOpenFilterDialog] = React.useState(false);
+
+  /////getData
+  const { loading, error, data } = useQuery(GET_CHARACTERS);
+  if (error) {
+    return <p>Error :</p>;
+  } else if (loading) {
+    return <p>Loading ...</p>;
+  }
+  const characters: Character[] = data.characters.results;
 
   /////handler//////
 
@@ -30,7 +57,7 @@ export default function CharacterList() {
 
   return (
     <>
-      <Box>
+      <Box sx={{ backgroundImage: `url(${background})` }}>
         <Box
           sx={{ display: "flex", justifyContent: "space-around", mt: "30px" }}
         >
@@ -64,6 +91,47 @@ export default function CharacterList() {
             </IconButton>
           </Box>
         </Box>
+
+        <Grid container spacing={6}>
+          {characters.map((character) => (
+            <Grid item xs={6} sm={4} md={3} key={character.id}>
+              <Box
+                sx={{
+                  border: "3px solid black",
+                  borderRadius: "20px",
+                  width: "300px",
+                  height: "302px",
+                  mt: "60px",
+                }}
+              >
+                <img
+                  src={character.image}
+                  alt={character.name}
+                  style={{ objectFit: "cover", borderRadius: "20px" }}
+                />
+              </Box>
+              <Box
+                sx={{ display: "flex", justifyContent: "center", mt: "10px" }}
+              >
+                <Box
+                  sx={{
+                    backgroundColor: `${theme.palette.secondary.main}`,
+                    width: "200px",
+                    height: "60px",
+                    borderRadius: "20px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography sx={{ fontSize: "18px", fontWeight: 700 }}>
+                    {character.name}
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
 
       {/* Dialog For Filter */}
