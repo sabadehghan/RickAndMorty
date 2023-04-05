ff1d495a4db65404dd1287214c78a07a3ede2043
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-05 19:59:46 +0430

    create graphql component

---
+++ src/graphql.tsx
@@ -0,0 +1,15 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache()
+});
+
+type GraphQLProviderProps = {
+  children: React.ReactNode;
+}
+
+export const GraphQLProvider: React.FC<GraphQLProviderProps> = ({ children }) => (
+  <ApolloProvider client={client}>{children}</ApolloProvider>
+);
