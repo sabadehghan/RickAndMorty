f4897f3a9fda8eca7b7e35e2073170e60918469a
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-05 15:04:11 +0430

    add handler (close & open) for filter dialog

--- src/components/characterList/CharacterList.tsx
+++ src/components/characterList/CharacterList.tsx
@@ -9,16 +9,33 @@ import {
 import SearchIcon from "@mui/icons-material/Search";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import { useTheme } from "@emotion/react";
+import FilterDialog from "../filterDialog/FilterDialog";
 
 export default function CharacterList() {
   const theme: any = useTheme();
 
+  
+  /////State//////
+  const [openFilterDialog, setOpenFilterDialog] = React.useState(false);
+
+  /////handler//////
+
+  const handleOpenFilterDialog = () => {
+    setOpenFilterDialog(true);
+  };
+
+  const handleCloseFilterDialog = () => {
+    setOpenFilterDialog(false);
+  };
+
   return (
     <>
       <Box>
-        <Box sx={{display: "flex", justifyContent: "space-around", mt: "30px"}}>
-          <Typography sx={{fontSize: "50px"}}>Rick and Morty</Typography>
-          <Box sx={{mt: "13px"}} >
+        <Box
+          sx={{ display: "flex", justifyContent: "space-around", mt: "30px" }}
+        >
+          <Typography sx={{ fontSize: "50px" }}>Rick and Morty</Typography>
+          <Box sx={{ mt: "13px" }}>
             <OutlinedInput
               sx={{
                 width: "476px",
@@ -35,13 +52,22 @@ export default function CharacterList() {
               }
             />
             <IconButton
-              sx={{ backgroundColor: `${theme.palette.primary.main}`, width: "63px" , height: "60px" , ml:"10px"}}
+              sx={{
+                backgroundColor: `${theme.palette.primary.main}`,
+                width: "63px",
+                height: "60px",
+                ml: "10px",
+              }}
+              onClick={handleOpenFilterDialog}
             >
-              <FilterListIcon sx={{fontSize:"25px"}} />
+              <FilterListIcon sx={{ fontSize: "25px" }} />
             </IconButton>
           </Box>
         </Box>
       </Box>
+
+      {/* Dialog For Filter */}
+      <FilterDialog open={openFilterDialog} onClose={handleCloseFilterDialog} />
     </>
   );
 }
