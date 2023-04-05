899e63976830ef5d9c50509ccefcb989fe2d32f3
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-05 14:51:05 +0430

    add palette color theme & fontFamily

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import MainRouter from "./router/MainRouter";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./assets/css/Font.css";
 
 function App() {
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          primary: {
+            main: "#A5D2E9",
+          },
+          secondary: {
+            main: "#B7B936",
+          },
+        },
+        typography: {
+          fontFamily: "IrishGrover",
+          allVariants: {
+            fontWeight: 500,
+          },
+        },
+      }),
+    []
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <MainRouter />
+      </ThemeProvider>
+    </>
   );
 }
 
