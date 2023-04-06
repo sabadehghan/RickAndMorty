e340dd61f8f751cfee05c4686f551e857c6af33e
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-07 00:06:40 +0430

    fix mainRouter

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CharacterList from "../components/characterList/CharacterList";
 
+
 export default function MainRouter() {
   return (
     <>
