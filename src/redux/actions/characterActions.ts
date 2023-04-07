de41b596b9fbe839d839a21f3d84b10133b53530
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-07 20:49:03 +0430

    change query name in characterActions

--- src/redux/actions/characterActions.ts
+++ src/redux/actions/characterActions.ts
@@ -1,18 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 // import { apolloClient } from "../../graphql/apolloClient";
-import { GET_CHARACTERS } from "../../graphql/character";
-import { selectCharacterList, actions } from "../slice/character";
+import { GUERY_CHARACTERS } from "../../graphql/character";
+import { selectCharacterList, actions } from "../slice/characterSlice";
 import { client } from "../../graphql/apolloClient";
 export default function useCharacter() {
-  const dispatch : any = useDispatch();
-  const characterList : any = useSelector(selectCharacterList);
+  const dispatch: any = useDispatch();
+  const characterList: any = useSelector(selectCharacterList);
 
-  const getCharacterList = async() => {
-        const response = await client.query({
-                query: GET_CHARACTERS
-        })
-   
-        dispatch(actions.setCharacterList(response.data.characters.results))
-        return {getCharacterList, characterList}
-  }
+  const getCharacterList = async () => {
+    const query = GUERY_CHARACTERS();
+    const response = await client.query({
+      query: query,
+    });
+
+    dispatch(actions.setCharacterList(response.data.characters.results));
+    return { getCharacterList, characterList };
+  };
 }
