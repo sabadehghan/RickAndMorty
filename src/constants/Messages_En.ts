8e987f46d085189294808b548e89fbbc7a7a2f30
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-07 21:39:46 +0430

    add messages file

---
+++ src/constants/Messages_En.ts
@@ -0,0 +1,20 @@
+// ################# //
+// #### ENGLISH #### //
+// ################# //
+
+export default {
+  RickAndMorty: "Rick And Morty",
+  Gender: "Gender",
+  Search: "Search",
+  Status: "Status",
+  Species: "Species",
+  None: "None",
+  Alive: "Alive",
+  Dead: "Dead",
+  Unknown: "Unknown",
+  Human: "Human",
+  Alien: "Alien",
+  Filter: "Filter",
+  Male: "Male",
+  Female: "Female"
+};
