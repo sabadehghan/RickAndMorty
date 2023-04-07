3df05c24338863e0860437686f305d2bb82b8418
sabadehghan <103627079+sabadehghan@users.noreply.github.com>
2023-04-07 21:40:36 +0430

    implement configure i18n

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,24 @@ import MainRouter from "./router/MainRouter";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./assets/css/Font.css";
 import { GraphQLProvider } from "./graphql/apolloClient";
+import i18n from "i18next";
+import * as I18next from "react-i18next";
+import Messages_En from "./constants/Messages_En";
+// Configure i18n //
+const i18nLanguageResources = {
+  en: {
+    translation: Messages_En,
+  },
+};
 
-
+i18n.use(I18next.initReactI18next).init({
+  resources: i18nLanguageResources,
+  lng: "fa",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 function App() {
   const theme = React.useMemo(
     () =>
